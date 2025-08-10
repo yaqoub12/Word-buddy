ecb34be1513a48a81b9eb360eca01896e92b0451
yaqoub12 <yaqoub12@gmail.com>
2025-08-10 07:37:27 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'wordbuddy-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './data.json'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
